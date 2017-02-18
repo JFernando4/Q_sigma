5e9822d044d0498563bfc1b9371ecb2da0dbbab8
Jfernando4 <jfhernan@ualberta.ca>
2017-02-17 19:23:14 -0700

    Minor Changes

--- Experiments.py
+++ Experiments.py
@@ -3,7 +3,7 @@ from Q_sigma import QSigma
 from MountainCar import MountainCar
 from joblib import Parallel, delayed
 import pickle
-from pylab import arange, zeros, size, float64
+from pylab import size
 
 
 def train_agent(environment, fa, sig, alph, n, bet, eps, episodes):
@@ -12,32 +12,34 @@ def train_agent(environment, fa, sig, alph, n, bet, eps, episodes):
     return agent
 
 if __name__ == '__main__':
-    num_agents = 200
-    alphas = arange(0.1, 1.1, 0.1)
-    sigmas = arange(0, 1.2, 0.2)
-    num_episodes = 50
-    num_cores = 10
+    num_agents = 100
+    alphas = [1/10, 1/9, 1/8, 1/7, 1/6, 1/5, 1/4, 1/3, 1/2, 1]
+    alpha_labels = ['10', '9', '8', '7', '6', '5', '4', '3', '2', '1']
+    sigmas = [0, 0.25, 0.50, 0.75, 1]
+    num_episodes = 100
+    num_cores = 4
     eps = 0.1
-    n = 1
+    n = 4
     Environment = MountainCar()
-    Func_Approx = TileCoderApproximatorAV()
 
     beta = 0.95
     for i in range(size(alphas)):
         alpha = alphas[i]
+        alpha_label = alpha_labels[i]
         agents = Parallel(n_jobs=num_cores)(delayed(train_agent)
-                                            (Environment, Func_Approx, 1, alpha, n, beta, eps, num_episodes)
+                                            (Environment, TileCoderApproximatorAV(), 1, alpha, n, beta, eps, num_episodes)
                                             for i in range(num_agents))
         pickle.dump(agents,
-                    open("/home/jfernan/Q_sigma/Agents/S1.0"+"_A"+str(alpha)+"_N"+str(n) + \
-                         "_B"+str(beta)+".p", "wb"))
+                    open("/home/jfernando/PycharmProjects/Q_sigma/S1.0"+"_A"+alpha_label+"_N"+str(n) + "_B"+str(beta)+\
+                         ".p", "wb"))
 
     for sigma in sigmas:
         for i in range(size(alphas)):
             alpha = alphas[i]
+            alpha_label = alpha_labels[i]
             agents = Parallel(n_jobs=num_cores)(delayed(train_agent)
-                                                (Environment, Func_Approx, sigma, alpha, n, 1, eps, num_episodes)
-                                                for i in range(num_agents))
+                                             (Environment, TileCoderApproximatorAV(), sigma, alpha, n, 1, eps, num_episodes)
+                                             for i in range(num_agents))
             pickle.dump(agents,
-                        open("/home/jfernan/Q_sigma/Agents/S"+str(sigma)+"_A"+str(alpha)+"_N"+str(n) + \
-                             "_B1.p", "wb"))
+                     open("/home/jfernando/PycharmProjects/Q_sigma/S"+str(sigma)+"_A"+alpha_label+"_N"+str(n) + \
+                         "_B1.p", "wb"))
--- Q_Sigma_Plots.py
+++ Q_Sigma_Plots.py
@@ -1,39 +1,194 @@
 import pickle
-
 import matplotlib.pyplot as plt
-from pylab import arange, size
-
-from Old.TileCoder import numTilings
-
-
-def average_reward_read_data(n=1):
-    alphas = arange(0.05, 1.05, 0.05) / numTilings
-    sigmas = arange(0, 1.2, 0.2)
-    Nstring = "N"+str(n) if n > 1 else ""
-    data = []
-    for sigma in sigmas:
-        sigma_results = []
-        for alpha in alphas:
-            alpha_data = pickle.load(open("Rewards_Sigma"+str(sigma)+"_Alpha"+str(alpha)+Nstring+".p", 'rb'))
-            results = 0
-            for agent_data in alpha_data:
-                results += sum(agent_data)/size(alpha_data)
-            sigma_results.append(results)
-        data.append(sigma_results)
-    return data
+from pylab import size, vstack, array, log, sort, inf, zeros, asarray, str_, argmax
+from os import listdir
 
 
-def plot_average_reward(n=1):
-    # x should be an n by m matrix or array where n is the number of observations and m is the number of episodes
-    data = average_reward_read_data(n)
-    alphas = arange(0.05, 1.05, 0.05)
-    sigmas = arange(0, 1.2, 0.2)
-    colors = ['k', 'b', 'g', 'red', 'c', 'm', 'y']
-    handles = []
-    for i in range(size(sigmas)):
-        plt.plot(alphas, data[i], color=colors[i])
-        handles.append(plt.Line2D([], [], color=colors[i], label=r'$\sigma$ ='+str(sigmas[i])))
-    plt.legend(handles=handles, loc=4)
+def read_letter_until_character(input_string, initial_index, character):
+    """ Reads and store each letter of a word until encounter an specified character """
+    mystring = ''
+    for i in range(initial_index, len(input_string)):
+        letter = input_string[i:(i+len(character))]
+        if letter != character:
+            mystring += input_string[i]
+        else:
+            break
+    return mystring
 
-def standard_plot(x, y, ylim=None, xlim=None, colors=None, legend=False, legend_labels=None, markers=None, lty=None):
-    pass
+
+def create_table_for_average_return(agents_directory):
+    myFiles = listdir(agents_directory)
+    arr = None
+    sigma = 0
+    alpha = 0
+    n = 0
+    beta = 0
+    for onefile in myFiles:
+        for k in range(len(onefile)):
+            if onefile[k] == 'S':
+                sigma = round(float(read_letter_until_character(onefile, k+1, '_')), 2)
+            elif onefile[k] == 'A':
+                alpha = int(read_letter_until_character(onefile, k+1, '_'))
+            elif onefile[k] == 'N':
+                n = round(int(read_letter_until_character(onefile, k+1, '_')), 1)
+            elif onefile[k] == 'B':
+                beta = round(float(read_letter_until_character(onefile, k+1, '.p')), 2)
+
+        agents = pickle.load(open(agents_directory+onefile, 'rb'))
+        average = 0
+        for agent in agents:
+            average += (sum(agent.return_per_episode) / agent.episode_number) / size(agents)
+
+        dtype = [('File_Name', str_, 20), ('n', int), ('sigma', float), ('beta', float), ('alpha', int),
+                 ('Average_Return', float)]
+        if arr is None:
+            arr = array((onefile, n, sigma, beta, alpha, average), dtype=dtype)
+        else:
+            arr = vstack([arr, array((onefile, n, sigma, beta, alpha, average), dtype=dtype)])
+    return arr
+
+
+def plot_average_return_per_episode(data, n, sigmas=None, betas=None, nlabel=False, lnstyle=None):
+    if sigmas is None: sigmas = [0, 0.25, 0.5, 0.75, 1]
+    lines = []
+    if betas is None: betas = [0.95, 1]
+    number_of_lines = 0
+    labels = []
+    min_return = inf
+    for beta in betas:
+        for sigma in sigmas:
+            temp_lines = [-inf for _ in range(10)]
+            temp_data = sort(data[(data['sigma'] == sigma)*(data['n'] == n)*(data['beta'] == beta)], order='alpha')
+            if size(temp_data) > 0:
+                offset = size(temp_lines) - size(temp_data['Average_Return'])
+                for i in range(size(temp_data)):
+                    temp_lines[i+offset] = temp_data['Average_Return'][i]
+                    if temp_data['Average_Return'][i] < min_return: min_return = temp_data['Average_Return'][i]
+                lines.append(temp_lines)
+                temp_label = r'$\sigma$ = '+str(sigma)
+                if beta != 1: temp_label += r', $\beta$ = '+str(beta)
+                if nlabel: temp_label += ", n = "+str(n)
+                labels.append(temp_label)
+                number_of_lines += 1
+
+    colors = ['b', 'g', 'c', 'k', 'y', 'm', 'b']
+    alphas = [i+1 for i in range(10)]
+    if lnstyle is None:
+        linestyle = (['solid'] * 5)
+        linestyle.append('dashed')
+    else: linestyle = [lnstyle]*5
+
+    for i in range(number_of_lines):
+        plt.plot(alphas, lines[i], color=colors[i], linestyle=linestyle[i], label=labels[i])
+
+    plt.legend(loc=4)
+    plt.xlabel(r'$1/\alpha$', fontsize=14)
+    plt.ylabel('Average Return per Episode', fontsize=14)
+    plt.ylim(ymin=min_return + 100, )
+
+
+def plot_return_per_episode(file_names, agents_dir, logscale=True, nlabel=True, alpha_label=True):
+    all_returns = []
+    labels =[]
+    total_episodes = []
+    for onefile in file_names:
+        agents = pickle.load(open(agents_dir+onefile, 'rb'))
+        number_of_episodes = agents[0].episode_number
+        total_episodes.append([i+1 for i in range(number_of_episodes)])
+        temp_returns = zeros(number_of_episodes)
+        for agent in agents:
+            if logscale: temp_returns += log(-asarray(agent.return_per_episode))
+            else: temp_returns += -asarray(agent.return_per_episode)
+        all_returns.append(temp_returns/size(agents))
+
+        sigma = 0; beta = 0; alpha = 0; n = 0;
+
+        for k in range(len(onefile)):
+            if onefile[k] == 'S':
+                sigma = round(float(read_letter_until_character(onefile, k+1, '_')), 2)
+            elif onefile[k] == 'A':
+                alpha = int(read_letter_until_character(onefile, k+1, '_'))
+            elif onefile[k] == 'N':
+                n = round(int(read_letter_until_character(onefile, k+1, '_')), 1)
+            elif onefile[k] == 'B':
+                beta = round(float(read_letter_until_character(onefile, k+1, '.p')), 2)
+
+        temp_label = r'$\sigma$ = '+str(sigma)
+        if beta != 1: temp_label += r', $\beta$ = '+str(beta)
+        if alpha_label: temp_label += r', $\alpha$ = '+str(alpha)
+        if nlabel: temp_label += ', n = '+str(n)
+
+        labels.append(temp_label)
+
+    colors = ['b', 'g', 'c', 'k', 'y', 'm', 'b']
+    for i in range(size(file_names)):
+        plt.plot(total_episodes[i], all_returns[i], color=colors[i], label=labels[i])
+
+    plt.legend(loc=1)
+    plt.xlabel('Episode Number', fontsize=14)
+    plt.ylabel('Average Return per Episode', fontsize=14)
+
+
+def plot_running_average(file_names, agents_dir, window=10, nlabel=True, alpha_label=True):
+    running_average = []
+    labels =[]
+    total_episodes = []
+    for onefile in file_names:
+        agents = pickle.load(open(agents_dir+onefile, 'rb'))
+        number_of_episodes = agents[0].episode_number
+        temp_returns = zeros(number_of_episodes)
+        for agent in agents:
+            temp_returns += -asarray(agent.return_per_episode)
+        temp_returns /= size(agents)
+        temp_running_average = []
+        for i in range(int(number_of_episodes/window)):
+            temp_running_average.append(sum(temp_returns[0:((i+1)*window)])/((i+1)*window))
+        total_episodes.append([(i+1)*window for i in range(int(number_of_episodes/window))])
+        running_average.append(temp_running_average)
+
+        sigma = 0; beta = 0; alpha = 0; n = 0;
+
+        for k in range(len(onefile)):
+            if onefile[k] == 'S':
+                sigma = round(float(read_letter_until_character(onefile, k+1, '_')), 2)
+            elif onefile[k] == 'A':
+                alpha = int(read_letter_until_character(onefile, k+1, '_'))
+            elif onefile[k] == 'N':
+                n = round(int(read_letter_until_character(onefile, k+1, '_')), 1)
+            elif onefile[k] == 'B':
+                beta = round(float(read_letter_until_character(onefile, k+1, '.p')), 2)
+
+        temp_label = r'$\sigma$ = '+str(sigma)
+        if beta != 1: temp_label += r', $\beta$ = '+str(beta)
+        if alpha_label: temp_label += r', $\alpha$ = '+str(alpha)
+        if nlabel: temp_label += ', n = '+str(n)
+
+        labels.append(temp_label)
+
+    colors = ['b', 'g', 'c', 'k', 'y', 'm', 'b']
+    for i in range(size(file_names)):
+        plt.plot(total_episodes[i], running_average[i], color=colors[i], label=labels[i])
+
+    plt.legend(loc=1)
+    plt.xlabel('Episode Number', fontsize=14)
+    plt.ylabel('Average Return at Episode Number', fontsize=14)
+
+
+
+
+def get_maximum(data, sigmas=None, betas=None):
+    if sigmas is None: sigmas = [0, 0.25, 0.5, 0.75, 1]
+    if betas is None: betas = [0.95, 1]
+
+    file_names = []
+    for beta in betas:
+        for sigma in sigmas:
+            temp_data = data[ (data['sigma']==sigma)*(data['beta']==beta) ]
+            if size(temp_data) != 0:
+                max_indx = argmax(temp_data['Average_Return'])
+                file_names.append(temp_data['File_Name'][max_indx])
+
+    return file_names
+
+
+
